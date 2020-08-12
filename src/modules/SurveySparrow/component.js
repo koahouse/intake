0e6516c8f3f930fd7589cd4d5d7b716bb529b55e
Marc <mwgdavies@gmail.com>
2020-08-12 12:10:46 +0200

    Change survey sparrow url to oliva

--- src/modules/SurveySparrow/component.js
+++ src/modules/SurveySparrow/component.js
@@ -28,8 +28,8 @@ export const Component = ({ onSubmit }) => {
   const src = useMemo(
     () =>
       languageCode === 'es'
-        ? `https://koa.surveysparrow.com/widget/intake--spanish/tt-246a95?id=${ID}`
-        : `https://koa.surveysparrow.com/widget/intake--english/tt-5031b3?id=${ID}`,
+        ? `https://oliva.surveysparrow.com/widget/intake--spanish/tt-246a95?id=${ID}`
+        : `https://oliva.surveysparrow.com/widget/intake--english/tt-5031b3?id=${ID}`,
     [languageCode]
   );
 
