897a769f16e5635694b2e3535409812dbd5dfa1a
Marc <mwgdavies@gmail.com>
2020-08-17 13:21:49 +0200

    Ignore Acuity load event when calendar is reloaded

--- src/modules/Acuity/component.js
+++ src/modules/Acuity/component.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import { useLanguageCode } from '../I18n';
 
@@ -8,16 +8,22 @@ import styles from './styles.module.css';
 export const Component = ({ onSubmit, responseId }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [isVisible, setIsVisible] = useState(true);
+  const isFinishedTimeout = useRef(null);
   const languageCode = useLanguageCode();
 
   const handleMessage = (event) => {
     if (!event || !event.data || !event.origin.includes('acuity')) return;
 
+    if (isFinishedTimeout.current)
+      window.clearTimeout(isFinishedTimeout.current);
+
     if (event.data.includes('sizing')) setIsLoaded(true);
 
     if (event.data.includes('load:')) {
-      setIsVisible(false);
-      onSubmit();
+      isFinishedTimeout.current = window.setTimeout(() => {
+        setIsVisible(false);
+        onSubmit();
+      }, 500);
     }
   };
 
