14e2e2468a37f3d3612f69c1a44818cae53fb489
Marc <mwgdavies@gmail.com>
2020-08-19 14:54:49 +0200

    Use custom Acuity fields

--- src/modules/Acuity/startAcuity.js
+++ src/modules/Acuity/startAcuity.js
@@ -1,9 +1,21 @@
 export const startAcuity = (languageCode, responseId) => {
   const iframe = window.document.createElement('iframe');
 
-  const owner = languageCode.includes('es') ? 20408348 : 20088051;
+  const { owner, idFieldName, linkFieldName } = languageCode.includes('es')
+    ? {
+        owner: 20408348,
+        idFieldName: 'field:8295360',
+        linkFieldName: 'field:8295365',
+      }
+    : {
+        owner: 20088051,
+        idFieldName: 'field:8295421',
+        linkFieldName: 'field:8295422',
+      };
 
-  iframe.src = `https://app.acuityscheduling.com/schedule.php?owner=${owner}&phone=${responseId}`;
+  iframe.src = `https://app.acuityscheduling.com/schedule.php?owner=${owner}&${idFieldName}=${responseId}&${linkFieldName}=${window.encodeURIComponent(
+    `https://sanc.typeform.com/to/lXfBjpxR#memberid=${responseId}`
+  )}`;
 
   const script = window.document.createElement('script');
 
