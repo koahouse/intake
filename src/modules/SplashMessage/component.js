04b5c8f39aeaa8698fb074bddd3ca8195776821d
Marc <mwgdavies@gmail.com>
2020-08-31 17:44:03 +0200

    Remove narrative copy from action container

--- src/modules/SplashMessage/component.js
+++ src/modules/SplashMessage/component.js
@@ -1,26 +1,17 @@
 import React from 'react';
 
 import { useIsMobile } from '../../utils';
-import { Button, Subheading } from '../ui';
-import { useStrings } from '../I18n';
+import { Button } from '../ui';
 
 import styles from './styles.module.css';
 
 export const Component = ({ onFinish }) => {
   const isMobile = useIsMobile();
-  const { THIS_WILL_TAKE_FIVE_TO_TEN_MINUTES } = useStrings();
 
   if (isMobile) return null;
 
   return (
     <div className={styles.container}>
-      <Subheading isCentered={!isMobile}>
-        To help you make the most of your intro meeting, we’ll start by getting
-        to know you with a few questions.
-      </Subheading>
-      <Subheading isCentered={!isMobile}>
-        {THIS_WILL_TAKE_FIVE_TO_TEN_MINUTES}
-      </Subheading>
       <Button isCentered={!isMobile} onClick={onFinish}>
         Get started
       </Button>
