c150bcc6d860844137509e012d7cde2949f95eaf
Marc <mwgdavies@gmail.com>
2020-09-04 12:57:51 +0200

    Trigger mobile detection on load event

--- src/utils/useIsMobile.js
+++ src/utils/useIsMobile.js
@@ -15,9 +15,13 @@ export const useIsMobile = () => {
   );
 
   useEffect(() => {
+    window.addEventListener('load', handleResize);
     window.addEventListener('resize', handleResize);
 
-    return () => window.removeEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('load', handleResize);
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
   return isMobile;
