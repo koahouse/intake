fe8b5d90499a3ecbac324624edf72229c2b1a3db
Marc <mwgdavies@gmail.com>
2020-09-21 13:52:02 +0200

    Create dropdown ui component

---
+++ src/modules/ui/Dropdown/component.js
@@ -0,0 +1,38 @@
+import React, { Fragment, useState, useEffect, useRef } from 'react';
+import ReactDropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+import styles from './styles.module.css';
+
+export const Component = ({ onChange, options, value }) => {
+  const [isMenuRightAligned, setIsMenuRightAligned] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    if (!ref.current || !ref.current.getBoundingClientRect) return;
+
+    const availableSpace =
+      window.innerWidth - ref.current.getBoundingClientRect().x;
+
+    setIsMenuRightAligned(availableSpace < 130);
+  }, [ref.current]);
+
+  return (
+    <Fragment>
+      <span ref={ref} />
+      <ReactDropdown
+        arrowClassName={styles.dropdownArrow}
+        className={styles.dropdownRoot}
+        controlClassName={styles.dropdownControl}
+        menuClassName={`${styles.dropdownMenu} ${
+          isMenuRightAligned && styles.isMenuRightAligned
+        }`}
+        onChange={onChange}
+        options={options}
+        value={value}
+      />
+    </Fragment>
+  );
+};
+
+Component.displayName = 'Dropdown';
--- src/modules/ui/index.js
+++ src/modules/ui/index.js
@@ -1,4 +1,5 @@
 export { Button } from './Button';
+export { Dropdown } from './Dropdown';
 export { SmallPrint } from './SmallPrint';
 export { Subheading } from './Subheading';
 export { Viewport } from './Viewport';
