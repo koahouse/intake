839fd11e2382a22b5af644ec0b3b456453834e2c
Marc <mwgdavies@gmail.com>
2020-10-12 12:34:35 +0200

    Fix warning in SurveySparrow

--- src/modules/SurveySparrow/component.js
+++ src/modules/SurveySparrow/component.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { v4 as uuid } from 'uuid';
 
 import { useLanguageCode } from '../I18n';
-import { useIsIndividual } from '../Pricing';
 
 import { getIsIndividual } from './utils/getIsIndividual';
 import { getSrc } from './utils/getSrc';
@@ -12,28 +11,36 @@ const ID = uuid().replace(/-/g, '').substring(0, 8);
 
 export const Component = ({ onSubmit }) => {
   const languageCode = useLanguageCode();
-  const isIndividual = useIsIndividual();
+  const isIndividual = true;
 
-  const handleMessage = (event) => {
-    if (!event || !event.data) return;
+  const handleMessage = useCallback(
+    (event) => {
+      if (!event || !event.data) return;
 
-    if (event.data.type === 'surveyCompleted') {
-      onSubmit(ID, getIsIndividual(event.data));
-      return;
-    }
-  };
+      if (event.data.type === 'surveyCompleted') {
+        onSubmit(ID, getIsIndividual(event.data));
+        return;
+      }
+    },
+    [onSubmit]
+  );
 
   useEffect(() => {
     window.addEventListener('message', handleMessage);
 
     return () => window.removeEventListener('message', handleMessage);
-  }, []);
+  }, [handleMessage]);
 
   const src = getSrc(ID, languageCode, isIndividual);
 
   return (
     <div className={styles.container}>
-      <iframe allowFullScreen="" id="ss_widget_frame" src={src}></iframe>
+      <iframe
+        allowFullScreen=""
+        id="ss_widget_frame"
+        src={src}
+        title="questionnaire"
+      ></iframe>
     </div>
   );
 };
