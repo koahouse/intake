6f2da8a8db633ddead3f88751b35fb90672573ed
Marc <mwgdavies@gmail.com>
2020-10-13 15:01:01 +0200

    Add new spanish form id

--- src/modules/SurveySparrow/utils/getSrc.js
+++ src/modules/SurveySparrow/utils/getSrc.js
@@ -2,7 +2,7 @@ export const getSrc = (id, languageCode, isIndividual) =>
   [
     'https://oliva.surveysparrow.com/widget',
     languageCode.includes('es')
-      ? '/intake--spanish/tt-3ea586'
+      ? '/intake--spanish/tt-890999'
       : '/intake--english/tt-5031b3',
     `?id=${id}`,
     `&individual=${isIndividual}`,
