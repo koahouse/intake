9332d0829187c86a90f84395ce1365e1eb41e3e6
Marc <mwgdavies@gmail.com>
2020-10-14 13:10:39 +0200

    Change 9 to 12 in packs

--- src/modules/Pricing/utils/getPrice.js
+++ src/modules/Pricing/utils/getPrice.js
@@ -3,11 +3,11 @@ export const getPrice = (languageCode, pack) =>
     es: {
       3: '€255',
       6: '€480',
-      9: '€840',
+      12: '€840',
     },
     en: {
       3: '£375',
       6: '£690',
-      9: '£1,320',
+      12: '£1,320',
     },
   }[languageCode][pack]);
