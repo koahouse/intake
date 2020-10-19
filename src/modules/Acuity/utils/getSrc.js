4e6e2837229477eaba6b0ba54da2f908a6afdf0d
Marc <mwgdavies@gmail.com>
2020-10-19 15:48:05 +0200

    Remove report form link

--- src/modules/Acuity/utils/getSrc.js
+++ src/modules/Acuity/utils/getSrc.js
@@ -8,7 +8,6 @@ export const getSrc = (
     appointmentTypeCouple,
     appointmentTypeIndividual,
     idFieldName,
-    linkFieldName,
     owner,
   } = languageCode.includes('es')
     ? // Spanish
@@ -16,7 +15,6 @@ export const getSrc = (
         appointmentTypeCouple: 16266387,
         appointmentTypeIndividual: 16738020,
         idFieldName: 'field:8295360',
-        linkFieldName: 'field:8295365',
         owner: 20408348,
       }
     : // English
@@ -24,7 +22,6 @@ export const getSrc = (
         appointmentTypeCouple: 16735191,
         appointmentTypeIndividual: 15473079,
         idFieldName: 'field:8295421',
-        linkFieldName: 'field:8295422',
         owner: 20088051,
       };
 
@@ -35,9 +32,6 @@ export const getSrc = (
       isIndividual ? appointmentTypeIndividual : appointmentTypeCouple
     }`,
     `&${idFieldName}=${responseId}`,
-    `&${linkFieldName}=${window.encodeURIComponent(
-      `https://sanc.typeform.com/to/lXfBjpxR#memberid=${responseId}&languagecode=${languageCode}`
-    )}`,
     `&certificate=${certificate}`,
     `&firstName=${firstName}`,
     `&lastName=${lastName}`,
